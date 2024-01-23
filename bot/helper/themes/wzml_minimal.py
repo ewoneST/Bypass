6293fced9de22449116aebe212616a5b4c23495b
ST Thamizhan <130844687+ewoneST@users.noreply.github.com>
2024-01-23 19:05:24 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -2,10 +2,10 @@
 class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = 'Master'
-    ST_BN1_URL = 'https://t.me/BalaPriyanB'
-    ST_BN2_NAME = 'Updates'
-    ST_BN2_URL = 'https://t.me/TomenBots'
+    ST_BN1_NAME = '🧑‍💻 Master'
+    ST_BN1_URL = 'https://t.me/STThamizhan'
+    ST_BN2_NAME = '💫 Updates'
+    ST_BN2_URL = 'https://t.me/st_update'
     ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
 <b>Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
@@ -140,7 +140,7 @@ class WZMLStyle:
     SIZE =                  '↦ <b>Size: </b>{Size}\n'
     ELAPSE =                '↦ <b>Elapsed: </b>{Time}\n'
     MODE =                  '↦ <b>Mode: </b>{Mode}\n'
-    LINE =                  '−−−−−−−−≺@TomenBots≻\n'
+    LINE =                  '−−−−−−−−≺@st_update≻\n'
 
     # ----- LEECH -------
     L_TOTAL_FILES =         '↦ <b>Total Files: </b>{Files}\n'
